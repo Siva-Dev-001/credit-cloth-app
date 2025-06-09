622c3baae4e066ea075d9de16c05104eee8d3f40
Siva <144576260+Siva-Dev-001@users.noreply.github.com>
2025-06-09 21:27:10 +0530

    Update settings.py

--- skdresses/settings.py
+++ skdresses/settings.py
@@ -30,7 +30,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.216.22.62','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['13.126.22.62','localhost', '127.0.0.1']
 
 
 # Application definition
