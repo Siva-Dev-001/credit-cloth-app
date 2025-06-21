b2af889521c136579bf8c1f27c66945494654754
Siva <144576260+Siva-Dev-001@users.noreply.github.com>
2025-06-21 11:54:20 +0530

    feat(Logging)
    1. Added the logging script in settings file.

--- skdresses/settings.py
+++ skdresses/settings.py
@@ -184,3 +184,22 @@ CACHES = {
         'LOCATION': 'redis://127.0.0.1:6379/1',
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'django-debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
